09beeaba08382cd6994e16615f1fbfd5e8c3d89c
chenk <1510687833@qq.com>
2018-10-31 11:08:52 +0800

    update catch imgUrl

--- kty_by_num_update.py
+++ kty_by_num_update.py
@@ -9,6 +9,7 @@ import re
 import os
 import threading
 import time
+from bs4 import BeautifulSoup as bs
 
 path=os.path.abspath('.')
 
@@ -17,43 +18,49 @@ def open_url(url):
     req.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.221 Safari/537.36 SE 2.X MetaSr 1.0')
     page=urllib.request.urlopen(req)
     html=page.read().decode('utf-8')
-
+    #print(html)
     return html
 
 
 def get_img(html):
-    p=r'<img src="https([^"]+\.png)"'
-    q=r'<img src="https([^"]+\.jpg)"'
-    b=r'<a href="http://.*">.*</a>'
-    urllist=re.findall(b,html)
+    
+    soup=bs(html,'lxml')
+    urlList=soup.find_all('img',src=re.compile("https"))
+    imgList=[]
+    for url in urlList:
+        print(url.get('src').lstrip())
+        imgList.append(url.get('src').lstrip())
+    print("\n共"+str(len(imgList))+"张图片")
+    fileName=soup.find('a',href=re.compile("http://bs309.com/xe")).string
+    print("\nfileName:"+fileName)
+    
     os.chdir(path)
-    for i in urllist:
-        ii=i.split('>')[-2]
-        name=ii.split('<')[-2]
-    os.mkdir(name)
-    os.chdir(name)
-    imglist=re.findall(p,html)
-    imglist1=re.findall(q,html)
-    for ii in imglist1:
-        imglist.append(ii)
+    
+    os.mkdir(fileName)
+    os.chdir(fileName)
+    
     downloads = []
-    for each in imglist:
-        filename=each.split("/")[-1]
-        down = threading.Thread(target=download, args=['https'+each,filename])
+    for each in imgList:
+        imgName=each.split("/")[-1]
+        down = threading.Thread(target=download, args=[each,imgName])
         downloads.append(down)
         down.start()
     for each in downloads:
         each.join()
-        #urllib.request.urlretrieve('https'+each,filename,None)
-def download(url,filename):
-    print('\ndownloading:'+url)
-    urllib.request.urlretrieve(url,filename,None)
-    print('\n[download success]:'+url)
+
+def download(url,imgName):
+    print('\n[downloading:]'+imgName)
+    urllib.request.urlretrieve(url,imgName,None)
+    print('\n[download success]:'+imgName)
     
 if __name__=='__main__':
-    url='http://bs309.com/xe/data/'
-    num=input('页面最后一串代码：')
-    time_start=time.time()
-    get_img(open_url(url+num))
-    time_end=time.time()
-    print('time cost',time_end-time_start,'s')
+    print("#此程序用于下载http://bs309.com/xe/data中的图片#")
+    print("\n-----如：http://bs309.com/xe/data/938  输入938即可-----")
+    while(True):
+        url='http://bs309.com/xe/data/'
+        num=input('\n链接最后的数字：')
+        time_start=time.time()
+        get_img(open_url(url+num))
+        time_end=time.time()
+        print("\n-----success-----")
+        print('\ntime cost',time_end-time_start,'s')
